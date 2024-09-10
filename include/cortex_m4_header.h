36f26d334caba8dfa2bcf22d52526a3c596d73b4
panxinhao <xinhao.pan@pimchip.cn>
2024-09-10 17:27:10 +0800

    🎉 init: Initial project structure and build configuration

---
+++ include/cortex_m4_header.h
@@ -0,0 +1,6 @@
+#ifndef CORTEX_M4_HEADER_H
+#define CORTEX_M4_HEADER_H
+
+void cortex_m4_function();
+
+#endif // CORTEX_M4_HEADER_H
---
+++ src/cortex_m4_code.c
@@ -0,0 +1,5 @@
+#include "cortex_m4_header.h"
+
+void cortex_m4_function() {
+    // Cortex-M4 example functionality
+}
